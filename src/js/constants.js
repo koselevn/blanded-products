c05189b83c29037c410118cfd667f9db683e95a0
Nikita Koshelev <koshelevn21@gmail.com>
2025-08-16 11:40:16 +0200

    add product api

---
+++ src/js/constants.js
@@ -0,0 +1,10 @@
+export const API_BASE_URL = "https://dummyjson.com/";
+export const API_ENDPOINTS = {
+    PRODUCTS: "products",
+    PRODUCTS_BY_ID: "products/",
+    CATEGORY: "products/category-list",
+    PRODUCTS_BY_CATEGORY: "products/category/",
+    PRODUCTS_SEARCH: "products/search",
+};
+
+export const ITEMS_PER_PAGE = 12;
